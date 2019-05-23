62d5921b2d7f119a1a84e34ec159c695ddcc2f27
raberin <rmvaberin@gmail.com>
2019-05-22 20:04:21 -0700

    Added my table from sqlite, connected the table to my api - working

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express = require('express');
+const express = require("express");
+
+const accountsDb = require("./data/accounts-model.js");
 
 const server = express();
 
 // your code here
+server.get("/", (req, res, next) => {
+  res.send(`
+  <h1>Lambda Backend/h1>
+  <h2>This is Roenz's DB I Challenge</h2>
+  `);
+});
+
+server.get("/api/accounts", async (req, res, next) => {
+  try {
+    const accounts = await accountsDb.find();
+    res.status(200).json(accounts);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error retrieving accounts information"
+    });
+  }
+});
 
 module.exports = server;
